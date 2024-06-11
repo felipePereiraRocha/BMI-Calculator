55d97a8521aaebdc6d44fe5a20bb2a1697d561f4
felipePereiraRocha <160747507+felipePereiraRocha@users.noreply.github.com>
2024-06-11 13:47:14 -0300

    ported to sass

--- bmicalc/src/Calculator.tsx
+++ bmicalc/src/Calculator.tsx
@@ -5,6 +5,9 @@ export function Calculator() {
     const [weight, setWeight] = useState<number>(0);
     const [height, setHeight] = useState<number>(0);
     const [bmi, setBmi] = useState<number>(0);
+    const [weightSettings, setWeightSettings] = useState<boolean>(true);
+    const [heightSettings, setHeightSettings] = useState<boolean>(true);
+
     useEffect(() => {
         getBMI();
     }, [weight, height]);
@@ -15,10 +18,22 @@ export function Calculator() {
         localStorage.setItem("userBmi", `${bmi}`);
     }
 
+    function handleHeightMetric() {
+        setHeightSettings(!heightSettings);
+        console.log(heightSettings)
+    }
+    function handleWeightMetric() {
+        setWeightSettings(!weightSettings!);
+        console.log(weightSettings)
+    }
+
     const getBMI = () => {
 
-        setWeight(document.querySelector("#weight").value);
-        setHeight(document.querySelector("#height").value / 100);
+        setWeight(document.querySelector("#weight")!.value);
+        setHeight(document.querySelector("#height")!.value / 100);
+
+        weightSettings ? null : setWeight(prevWeight => weight * 2.205);
+        heightSettings ? null : setHeight(prevHeight => height *  30.48);
 
         setBmi(Math.round(weight / Math.pow(height, 2) * 10) / 10);
 
@@ -31,11 +46,11 @@ export function Calculator() {
     return (
         <div className="main-div">
             <h1 className="title">BMI CALCULATOR</h1>
-            <label className="labels" htmlFor="weight">Weight kg:</label>
-            <input defaultValue={0} type="number" id="weight" placeholder="70" min={0}></input>
-            <label className="labels" htmlFor="height">Height cm:</label>
+            <label className="labels" htmlFor="weight">Weight <button onClick={handleWeightMetric}>{weightSettings? "kg" : "lbs"}</button>:</label>
+            <input defaultValue={75} type="number" id="weight" placeholder="70" min={0}></input>
+            <label className="labels" htmlFor="height">Height <button onClick={handleHeightMetric}>{heightSettings? "cm" : "ft"}</button>:</label>
             <input defaultValue={0} type="number" id="height" placeholder="170" min={0}></input>
-            <button onClick={getBMI}>Get BMI</button>
+            <button  className="get-bmi-button" onClick={getBMI}>Get BMI</button>
             <h1 className="bmi-display">{bmi}</h1>
             <p>note that BMI(Body Mass Index) is not a definite measure of a person's health</p>
         </div>
