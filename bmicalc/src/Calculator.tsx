f0aa8496e92ee8aae2a950255d8e455950a90ebd
felipePereiraRocha <160747507+felipePereiraRocha@users.noreply.github.com>
2024-06-17 15:16:41 -0300

    UPDATE
    -Added feature that allows user to switch between unit systems;
    -Added functionality that automaticaly recalculates values upon unit change;

--- bmicalc/src/Calculator.tsx
+++ bmicalc/src/Calculator.tsx
@@ -5,8 +5,7 @@ export function Calculator() {
     const [weight, setWeight] = useState<number>(0);
     const [height, setHeight] = useState<number>(0);
     const [bmi, setBmi] = useState<number>(0);
-    const [weightSettings, setWeightSettings] = useState<boolean>(true);
-    const [heightSettings, setHeightSettings] = useState<boolean>(true);
+    const [unitSettings, setUnitSettings] = useState<boolean>(true);
 
     useEffect(() => {
         getBMI();
@@ -18,28 +17,43 @@ export function Calculator() {
         localStorage.setItem("userBmi", `${bmi}`);
     }
 
-    function handleHeightMetric() {
-        setHeightSettings(!heightSettings);
-        console.log(heightSettings)
-    }
-    function handleWeightMetric() {
-        setWeightSettings(!weightSettings!);
-        console.log(weightSettings)
-    }
+    function handleUnit() {
 
+        if(unitSettings === true) {
+            document.querySelector("#weight")!.value = Math.round(weight *  2.205);
+            document.querySelector("#height")!.value = Math.round(document.querySelector("#height")!.value / 2.54);
+        } else {
+            document.querySelector("#weight")!.value = Math.round(weight /  2.205);
+            document.querySelector("#height")!.value = Math.round(document.querySelector("#height")!.value * 2.54);
+        }
+
+        setUnitSettings(!unitSettings);
+        console.log(unitSettings);
+    }
     const getBMI = () => {
 
+        //IF Using metric unit system//
+        if(unitSettings === true) {
         setWeight(document.querySelector("#weight")!.value);
         setHeight(document.querySelector("#height")!.value / 100);
-
-        weightSettings ? null : setWeight(_prevWeight => weight / 2.205);
-        heightSettings ? null : setHeight(_prevHeight => height *  30.48);
 
         setBmi(Math.round(weight / Math.pow(height, 2) * 10) / 10);
 
         console.log(weight, height, bmi);
 
         saveUserStats();
+        } else {
+            //IF USING IMPERIAL UNIT SYSTEM//
+            setWeight(document.querySelector("#weight")!.value);
+            setHeight(document.querySelector("#height")!.value);
+
+            setBmi(Math.round(703 * (weight / (height ** 2))));
+
+            console.log(weight, height, bmi);
+
+            saveUserStats();
+
+        }
 
     };
 
@@ -47,14 +61,15 @@ export function Calculator() {
         if(event.key === "Enter") {
             getBMI();
         }
+        
     })
 
     return (
         <div className="main-div">
             <h1 className="title">BMI CALCULATOR</h1>
-            <label className="labels" htmlFor="weight">Weight <button onClick={handleWeightMetric}>{weightSettings? "kg" : "lbs"}</button>:</label>
+            <label className="labels" htmlFor="weight">Weight <button onClick={handleUnit}>{unitSettings ? "kg" : "lb"}</button>:</label>
             <input defaultValue={70} type="number" id="weight" placeholder="70" min={0} onChange={getBMI}></input>
-            <label className="labels" htmlFor="height">Height <button onClick={handleHeightMetric}>{heightSettings? "cm" : "ft"}</button>:</label>
+            <label className="labels" htmlFor="height">Height <button onClick={handleUnit}>{unitSettings ? "cm" : "in"}</button>:</label>
             <input defaultValue={170} type="number" id="height" placeholder="170" min={0} onChange={getBMI}></input>
             <button  className="get-bmi-button" onClick={getBMI}>Get BMI</button>
             <h1 className="bmi-display">{bmi}</h1>
